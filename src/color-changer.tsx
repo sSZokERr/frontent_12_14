cf78fd53e99ab8e647369e67127d45b4f618b731
szoker <alex@gmail.com>
2022-12-14 09:41:39 +0100

    init

---
+++ src/color-changer.tsx
@@ -0,0 +1,9 @@
+import { Component, ReactNode } from "react";
+class colorChanger extends Component{
+    render(): ReactNode {
+        return <div>
+            <p>Minta szöveg</p>
+            <button>Katt ide</button>
+        </div>
+    }
+}
